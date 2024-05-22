7efff3471365b322185cd5f2aaa86b0691ad61e8
Jonathan-555 <jonathanel28@gmail.com>
2024-05-22 09:45:17 +0200

    Primera subida git

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Punto de entrada de la aplicación
+        // Aquí puedes añadir el código que deseas ejecutar
+    }
+}
